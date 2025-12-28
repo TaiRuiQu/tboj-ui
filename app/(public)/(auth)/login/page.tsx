45190589c85a070475ab081331a0b1df7cf2c376
Tairui Qu <XiaoQuQu_CN@outlook.com>
2025-12-28 11:58:41 +0800

    fix(login-page): building errors

--- app/(public)/(auth)/login/page.tsx
+++ app/(public)/(auth)/login/page.tsx
@@ -1,10 +1,15 @@
 import { LoginPage } from '@/features/auth/login/login-page';
 import type { Metadata } from 'next';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: '登录',
 };
 
 export default function LoginRoutePage() {
-  return <LoginPage />;
+  return (
+    <Suspense fallback={null}>
+      <LoginPage />
+    </Suspense>
+  );
 }
